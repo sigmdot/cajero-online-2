d2ffa4ab4c6ca727a891cffec124bb3703fce0e0
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-06-16 17:26:23 -0400

    creada vista dashboard, y agregada a ruta

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="dashboardview">
+
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Dashboard',
+    components:{
+
+    },
+    data:function(){
+        return{
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
