8f4d5ff84f799dcd176ab19c81f74312524baf0d
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-06-16 17:50:26 -0400

    updated view

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="dashboardview">
-
+      <div class="container-fluid">
+          <div class="row m-0">
+              
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
+
+
+
 export default {
     name:'Dashboard',
     components:{
 
     },
     data:function(){
+
         return{
             
         }
