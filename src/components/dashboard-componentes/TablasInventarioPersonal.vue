04fb75dce6ee99cb23e1c3a9627dda3ad12487a6
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-06-16 18:50:04 -0400

    creado buttonbox, tablainventario personal con header

---
+++ src/components/dashboard-componentes/TablasInventarioPersonal.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="tablasinventariopersonal col-lg-12 p-0 row m-0">
+      <div class="col-lg-12 p-0 text-center border p-3">
+          <h4>{{title}}</h4>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name:'TablasInventarioPersonal',
+    components:{
+
+    },
+    data:function(){
+        return{
+
+        }
+    },
+    props:{
+        title:String
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,34 +1,40 @@
 <template>
   <div class="dashboardview">
-      <div class="container-fluid">
-          <div class="row m-0 justify-content-around">
-              <h3 class="col-12">Comparación</h3>
-              <ComparacionResumen titulo="Venta mensual" diferencia="99.999" evaluacion="up"></ComparacionResumen>
-              <ComparacionResumen titulo="Venta Semanal" diferencia="99.999" evaluacion="down"></ComparacionResumen>
-              <ComparacionResumen titulo="Clientes hoy" diferencia="99.999" evaluacion="draw"></ComparacionResumen>
-          </div>
+    <div class="container-fluid">
+      <div class="row m-0 justify-content-between">
+        <h3 class="col-12">Comparación</h3>
+        <ComparacionResumen titulo="Venta mensual" diferencia="99.999" evaluacion="up"></ComparacionResumen>
+        <ComparacionResumen titulo="Venta Semanal" diferencia="99.999" evaluacion="down"></ComparacionResumen>
+        <ComparacionResumen titulo="Clientes hoy" diferencia="99.999" evaluacion="draw"></ComparacionResumen>
       </div>
+      <div class="row m-0">
+          <div class="col-lg-12 row m-0 is-hidden-mobile p-0">
+              <ButtonBox class="mb-1"></ButtonBox>
+              <TablasInventarioPersonal title="Productos más vendido esta semana"></TablasInventarioPersonal>
+          </div>
+        
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
-import ComparacionResumen from '@/components/dashboard-componentes/ComparacionResumen.vue'
+import ComparacionResumen from "@/components/dashboard-componentes/ComparacionResumen.vue";
+import ButtonBox from "@/components/dashboard-componentes/ButtonBox.vue";
+import TablasInventarioPersonal from "@/components/dashboard-componentes/TablasInventarioPersonal.vue"
 
 export default {
-    name:'Dashboard',
-    components:{
-        ComparacionResumen
-    },
-    data:function(){
-
-        return{
-            
-        }
-    }
-}
+  name: "Dashboard",
+  components: {
+    ComparacionResumen,
+    ButtonBox,
+    TablasInventarioPersonal
+  },
+  data: function() {
+    return {};
+  }
+};
 </script>
 
 <style>
-
 </style>
