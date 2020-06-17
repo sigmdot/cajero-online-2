8e3b4dd4f0f95b2f66af861880de4fc22c5b7011
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-06-16 22:15:00 -0400

    creada vista Inventario

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Dashboard from '../views/Dashboard.vue'
+import InventarioView from '../views/InventarioView.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ Vue.use(VueRouter)
     path:'/dashboard',
     name: 'Dashboard',
     component: Dashboard
+  },
+  {
+    path:'/inventario',
+    name:'Inventario',
+    component: InventarioView
   }
 ]
 
---
+++ src/views/InventarioView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="inventarioview">
+      <div class="container-fluid">
+          
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'InventarioView',
+    components:{
+
+    },
+    data:function(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
