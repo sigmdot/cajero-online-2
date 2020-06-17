88c975f7c44c0be5be258e47b12801c6fb104a7a
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-06-16 20:44:27 -0400

    filastablas creada, actualizada buttonbox

--- src/components/dashboard-componentes/ButtonBox.vue
+++ src/components/dashboard-componentes/ButtonBox.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="col-12 col-sm-12 col-md-12 col-lg-2 ml-auto p-0 text-center buttonboxtable">
-          <div class="btn-group" role="group" aria-label="Basic example">
+  <div class="col-12 col-sm-12 col-md-12 col-lg-1 ml-auto p-0 text-center buttonboxtable">
+          <div class="btn-group w-100" role="group" aria-label="Basic example">
             <button type="button" class="btn btn-secondary btn-sm"> <b-icon icon="pie-chart-fill"></b-icon> </button>
             <button type="button" class="btn btn-secondary btn-sm"><b-icon icon="table"></b-icon></button>
           </div>
---
+++ src/components/dashboard-componentes/FilasTabla.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="filastabla">
+
+  </div>
+</template>
+
+<script>
+export default {
+    name:'FilasTabla',
+    components:{
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
