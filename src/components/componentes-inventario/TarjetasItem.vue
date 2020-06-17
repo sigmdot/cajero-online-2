fad507ef1d99f7337f961efc65985bc11f14c779
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-06-16 22:20:09 -0400

    creada tarjetas item, con dumbdata

---
+++ src/components/componentes-inventario/TarjetasItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="tarjetasitem col-lg-4 p-0 border">
+
+  </div>
+</template>
+
+<script>
+export default {
+    name:'TarjetasItem',
+    components:{
+
+    },
+    data:function () {
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/InventarioView.vue
+++ src/views/InventarioView.vue
@@ -1,25 +1,55 @@
 <template>
   <div class="inventarioview">
-      <div class="container-fluid">
-          
-      </div>
+    <div class="container-fluid">
+        <div class="row m-0">
+            
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name:'InventarioView',
-    components:{
-
-    },
-    data:function(){
-        return{
-
+  name: "InventarioView",
+  components: {
+      
+  },
+  data: function() {
+    return {
+      itemList: [
+        {
+          cod: "192a",
+          nombre: "Fideos Carozzi 1 kg",
+          dinero: 999,
+          cantidad: 99,
+          imgUrl:''
+        },
+        {
+          cod: "192b",
+          nombre: "Coca cola 3LT",
+          dinero: 1000000,
+          cantidad: 99,
+          imgUrl:''
+        },
+        {
+          cod: "192c",
+          nombre: "Desodorante en barra huele bonito",
+          dinero: 10000,
+          cantidad: 99,
+          imgUrl:''
+        },
+        {
+          cod: "192d",
+          nombre: "Helado Capri No capri , k es capri",
+          dinero: 8888,
+          cantidad: 99,
+          imgUrl:''
         }
-    }
-}
+      ]
+    };
+  }
+};
 </script>
 
 <style>
-
 </style>
