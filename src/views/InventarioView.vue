2ccefbeb438a2d437f8fede641efc04fd9a78d79
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-06-19 16:18:32 -0400

    borrada tarjeta view item

--- src/views/InventarioView.vue
+++ src/views/InventarioView.vue
@@ -1,21 +1,17 @@
 <template>
   <div class="inventarioview">
     <div class="container-fluid">
-        <div class="row m-0 justify-content-between">
-            <!-- <TarjetasItem v-for="(item,index) in itemList" :key="index" :item="item"></TarjetasItem> -->
-            
-        </div>
+
     </div>
   </div>
 </template>
 
 <script>
-/* import TarjetasItem from '@/components/componentes-inventario/TarjetasItem.vue' */
 
 export default {
   name: "InventarioView",
   components: {
-      /* TarjetasItem */
+      
   },
   data: function() {
     return {
@@ -61,7 +57,22 @@ export default {
           dinero: 8888,
           cantidad: 99,
           imgUrl:''
-        }
+        },
+        {
+          cod: "192d",
+          nombre: "Helado Capri No capri , k es capri",
+          dinero: 8888,
+          cantidad: 99,
+          imgUrl:''
+        },
+         {
+          cod: "192c",
+          nombre: "Desodorante en barra huele bonito",
+          dinero: 10000,
+          cantidad: 99,
+          imgUrl:''
+        },
+        
       ]
     };
   }
