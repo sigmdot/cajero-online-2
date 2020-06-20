c9d4761e72ea217a7f565087f6ef2734cdd54008
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-06-20 19:17:04 -0400

    actualizada la vista y agregado a rutas

---
+++ src/views/User/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="login container">
+      <div class="row m-0">
+          
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Login',
+    components:{
+
+    }
+}
+</script>
+
+<style>
+
+</style>
