e15732b682b643cbc3cb489ee3765e52508ba4fa
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-06-20 19:40:08 -0400

    Login actualizado

--- src/components/componentes-login/FormularioLogin.vue
+++ src/components/componentes-login/FormularioLogin.vue
@@ -1,13 +1,39 @@
 <template>
-  
+  <form class="formulariologin col-12 col-md-6 p-0 bg-white p-5 rounded">
+    <div class="form-group">
+      <label for="formGroupExampleInput"> <strong>Correo:</strong> </label>
+      <input
+        type="email"
+        class="form-control"
+        id="formGroupExampleInput"
+        placeholder="ejemplo@ejemplo.cl"
+      />
+    </div>
+    <div class="form-group">
+      <label for="formGroupExampleInput2"> <strong>Contraseña:</strong> </label>
+      <input
+        type="password"
+        class="form-control"
+        id="formGroupExampleInput2"
+        placeholder="Contraseña"
+      />
+    </div>
+    <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Recordarme</label>
+  </div>
+  <div class="col-12 row p-0 m-0 justify-content-center">
+      <button type="submit" class="btn btn-outline-primary w-100">Entrar</button>
+  </div>
+  </form>
 </template>
 
 <script>
 export default {
-
-}
+  name: "FormularioLogin",
+  components: {}
+};
 </script>
 
 <style>
-
 </style>
--- src/views/User/Login.vue
+++ src/views/User/Login.vue
@@ -1,20 +1,34 @@
 <template>
   <div class="login container">
-      <div class="row m-0">
-          
+      <div class="row m-0 justify-content-center">
+          <h5 class="col-12 text-center p-0">¡Bienvenido denuevo!</h5>
+          <FormularioLogin></FormularioLogin>
       </div>
   </div>
 </template>
 
 <script>
+import FormularioLogin from '@/components/componentes-login/FormularioLogin.vue'
+
 export default {
     name:'Login',
     components:{
-
+        FormularioLogin
     }
 }
 </script>
 
 <style>
+.bg {
+  /* The image used */
+  background-image: url("https://i.ibb.co/f1MFtSp/minimalist.jpg");
 
+  /* Full height */
+  height: 100%;
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
 </style>
