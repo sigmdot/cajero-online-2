2993b9e20172b3be2e191cf4aaff88e2460dd6df
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-06-21 04:24:01 -0400

    actualizado formulario registro

--- src/views/User/Register.vue
+++ src/views/User/Register.vue
@@ -1,16 +1,18 @@
 <template>
     <div class="registerview container">
-        <div class="row m-0">
-            
+        <div class="row m-0 justify-content-center">
+            <FormularioRegistro />
         </div>
     </div>
 </template>
 
 <script>
+import FormularioRegistro from '@/components/componentes-register/FormularioRegistro.vue'
+
 export default {
     name:'Register',
     components:{
-
+        FormularioRegistro
     },
     data:function(){
         return{
