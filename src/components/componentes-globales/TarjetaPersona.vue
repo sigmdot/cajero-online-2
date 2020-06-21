3af953bd64d1f55d8a0416d88e3689bc1cc09c92
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-06-21 19:03:19 -0400

    Creada tarjeta persona

---
+++ src/components/componentes-globales/TarjetaPersona.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="tarjetapersona col-lg-3">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:'TarjetaPersona',
+    components:{
+
+    },
+    data:function(){
+        return{
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
