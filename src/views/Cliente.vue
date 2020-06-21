b490b01748507657dada8d59534a41274575d151
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-06-21 18:56:57 -0400

    actualizada rutas y cliente view

---
+++ src/views/Cliente.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="clienteview">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:'ClienteView',
+    components:{
+
+    },
+    data:function(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
