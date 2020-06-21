4f83e5fe3b1a0c6d8e65fc3354705893fef41899
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-06-21 05:12:26 -0400

    actualizado fondo register, texto registro

--- src/views/User/Register.vue
+++ src/views/User/Register.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="registerview container">
         <div class="row m-0 justify-content-center">
+            <h3 class="col-12 p-0 text-center">Bienvenido a Caja online</h3>
+            <p class="col-12 p-0 text-center"> Antes de poder acceder a nuestra <strong>asombrosa aplicación</strong> te pediremos algunos datos </p>
             <FormularioRegistro />
         </div>
     </div>
